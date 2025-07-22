b6c1f4f3b71e325921e45064c1a2ea8fdb3fe9ad
“Piyush” <piyushchandra248@gmail.com>
2025-07-22 23:46:18 +0530

    some more clarity

---
+++ src/main/java/click/piyush/book/dto/UserRegisterResponse.java
@@ -0,0 +1,5 @@
+package click.piyush.book.dto;
+
+public record UserRegisterResponse(String userId, String email, String token) {
+    
+}
--- src/main/java/click/piyush/book/service/userService.java
+++ src/main/java/click/piyush/book/service/userService.java
@@ -3,10 +3,10 @@ package click.piyush.book.service;
 
 import click.piyush.book.dto.LoginRequest;
 import click.piyush.book.dto.LoginResponse;
-import click.piyush.book.dto.SignUpRequest;
-import click.piyush.book.dto.SignUpResponse;
+import click.piyush.book.dto.UserRegisterRequest;
+import click.piyush.book.dto.UserRegisterResponse;
 
-public interface userService {
-    public SignUpResponse userSignUp(SignUpRequest signUpRequest);
+public interface UserService {
+    public UserRegisterResponse userSignUp(UserRegisterRequest signUpRequest);
     public LoginResponse userLogin(LoginRequest loginRequest);
 }
